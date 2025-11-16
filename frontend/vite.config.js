7f851696c6ad4ff8201512237e9cc4f1746638a7
AJ <richie.okoro@gmail.com>
2025-11-16 15:28:04 +0100

    added allowed hosts in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		allowedHosts: [
+			'residence541.com',
+			'www.residence541.com',
+			'localhost:5173',
+			'127.0.0.1:5173'
+		]
+	}
 });
