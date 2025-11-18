93c8b2aabfed98814f5cce960582622e37290454
AJ <richie.okoro@gmail.com>
2025-11-18 08:53:18 +0100

    csrf control in svelte.config.js

--- frontend/src/routes/booking/+page.server.js
+++ frontend/src/routes/booking/+page.server.js
@@ -1,5 +1,7 @@
 export const actions = {
     default: async ({ request }) => {
+        console.log('Origin:', request.headers.get('origin'));
+        console.log('Host:', request.headers.get('host'));
         let message = "Nothing done"
         try {
             const formData = await request.formData();
@@ -29,6 +31,6 @@ export const actions = {
                 success: false, error: error.message
             }
         }
-        return { success: true, message}
+        return { success: true, message }
     }
 }
--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -2,6 +2,15 @@ import adapter from '@sveltejs/adapter-node';
 
 export default {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		csrf: {
+			checkOrigin: true,
+			allowedOrigins: [
+				'residence541.com',
+				'www.residence541.com',
+				'localhost:5173',
+				'127.0.0.1:5173'
+			]
+		}
 	}
 };
