a22fa009c021b7e769853f6ca097a50d71914544
AJ <richie.okoro@gmail.com>
2025-11-18 09:18:23 +0100

    removed all edits from svelte.config

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -2,15 +2,6 @@ import adapter from '@sveltejs/adapter-node';
 
 export default {
 	kit: {
-		adapter: adapter(),
-		csrf: {
-			checkOrigin: true,
-			allowedOrigins: [
-				'residence541.com',
-				'www.residence541.com',
-				'localhost:5173',
-				'127.0.0.1:5173'
-			]
-		}
+		adapter: adapter()
 	}
 };
