f083ba8727c113a7e55f24ee85a859cb3baa8434
AJ <richie.okoro@gmail.com>
2025-11-19 08:28:53 +0100

    improvement?

--- frontend/src/routes/send/+page.svelte
+++ frontend/src/routes/send/+page.svelte
@@ -13,6 +13,8 @@
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
             }
+            const data = await response.json()
+            console.log("data ->\n", data)
         } catch (error) {
             console.error("Error:", error);
         }
