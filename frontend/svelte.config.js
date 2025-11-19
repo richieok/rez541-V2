3369b7d7337ebb8f3633d95f75ded0731498bfd6
AJ <richie.okoro@gmail.com>
2025-11-19 09:06:51 +0100

    adapter changes

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -2,16 +2,10 @@ import adapter from '@sveltejs/adapter-node';
 
 export default {
 	kit: {
-		adapter: adapter(),
-		csrf: {
-			checkOrigin: true,
-			trustedOrigins: [
-				"www.residence541.com",
-				"residence541.com",
-				// "localhost:5173",
-				// "127.0.0.1:5173"
-			]
-		}
-		
+		adapter: adapter({
+			protocol_header: 'x-forwarded-proto',
+			host_header: 'x-forwarded-host'
+		})
+
 	}
 };
