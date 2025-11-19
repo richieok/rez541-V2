861c4fc98d3bc2a3f0b9c3d0b93d1c9dc0e36dfa
AJ <richie.okoro@gmail.com>
2025-11-19 18:11:18 +0100

    Removed the changes to svelte.config

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -2,10 +2,6 @@ import adapter from '@sveltejs/adapter-node';
 
 export default {
 	kit: {
-		adapter: adapter({
-			protocol_header: 'x-forwarded-proto',
-			host_header: 'x-forwarded-host'
-		})
-
+		adapter: adapter()
 	}
 };
