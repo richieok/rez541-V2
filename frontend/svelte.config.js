145365e0f72763351dc784f128bde72a42c3b5c1
AJ <richie.okoro@gmail.com>
2025-11-20 08:20:58 +0100

    believe csrf issue sorted by editing nginx.conf

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -2,14 +2,6 @@ import adapter from '@sveltejs/adapter-node';
 
 export default {
 	kit: {
-		adapter: adapter(),
-		csrf: {
-			trustedOrigins: [
-				"http://localhost:3000",
-				"http://localhost:3000",
-				"http://localhost:8080",
-				"http://127.0.0.1:8080",
-			]
-		}
+		adapter: adapter()
 	}
 };
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,14 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		// allowedHosts: [
-		// 	'residence541.com',
-		// 	'www.residence541.com',
-		// 	'localhost:5173',
-		// 	'127.0.0.1:5173'
-		// ],
-		// cors
-	}
+	plugins: [sveltekit()]
 });
