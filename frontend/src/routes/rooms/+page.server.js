0a83277f7fec77594dc6ca033bc104d4649e074c
AJ <richie.okoro@gmail.com>
2025-12-22 08:20:08 +0100

    Remove commented-out code for signed URL retrieval in room loading function

--- frontend/src/routes/rooms/+page.server.js
+++ frontend/src/routes/rooms/+page.server.js
@@ -3,15 +3,5 @@ import { getRooms } from "$lib/server/bookingApp.js"
 
 export async function load() {
     let rooms = await getRooms()
-    // console.log("Rooms count:", rooms.length)
-    // for (let room of rooms) {
-    //     const res = await retrieveSignedUrls(room.imageList)
-    //     const { signedUrls } = await res.json()
-    //     console.log("signedUrls ->", signedUrls)
-    //     room.signedUrls = signedUrls
-    // }
-    // const ans = await resp.json()
-    // console.log(ans);
-
     return { "rooms": rooms }
 }
